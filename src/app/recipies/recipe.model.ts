95ddbe95c2a3214f454679d84579b88546bfae3e
Aakash Chahal <aakash.chahal62@gmail.com>
2022-09-19 18:40:29 +0100

    adding recipe component

---
+++ src/app/recipies/recipe.model.ts
@@ -0,0 +1,11 @@
+export class Recipe {
+  public name: string;
+  public description: string;
+  public img: string;
+
+  constructor(name: string, desc: string, img: string) {
+    this.name = name;
+    this.description = desc;
+    this.img = img;
+  }
+}
