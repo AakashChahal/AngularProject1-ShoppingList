4c0d2116585f42a8106181549e963763d7e4293d
Aakash Chahal <aakash.chahal62@gmail.com>
2022-09-24 15:06:48 +0100

    updating shopping list logic

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -9,8 +9,9 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [new Ingredient('Test', 10)];
   updateShoppingList(newItem: Ingredient) {
-    this.ingredients.push(newItem);
-    console.log(this.ingredients);
+    newItem.name != '' && newItem.qty
+      ? this.ingredients.push(newItem)
+      : console.log();
   }
   constructor() {}
 
